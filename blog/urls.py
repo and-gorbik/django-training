0606f6638d70147ae6b183a4dc0c61176d4b241a
and-gorbik <and-gorbik@mail.ru>
2019-01-27 14:12:45 +0300

    add class-based views and pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,24 @@
 from django.urls import path
-from .views import article, articles, create_article
-from .views import update_article, delete_article
+# from .views import article, articles, create_article
+# from .views import update_article, delete_article
+
+# urlpatterns = [
+#     path('', articles, name='index'),
+#     path('articles/', articles, name='articles'),
+#     path('articles/new/', create_article, name='new_article'),
+#     path('articles/<str:slug>/', article, name='article'),
+#     path('articles/<str:slug>/update/', update_article, name='update_article'),
+#     path('articles/<str:slug>/delete/', delete_article, name='delete_article'),
+# ]
+
+from .views import ArticleDetail, ArticleList, ArticleCreate
+from .views import ArticleUpdate, ArticleDelete
 
 urlpatterns = [
-    path('', articles, name='index'),
-    path('articles/', articles, name='articles'),
-    path('articles/new/', create_article, name='new_article'),
-    path('articles/<str:slug>/', article, name='article'),
-    path('articles/<str:slug>/update/', update_article, name='update_article'),
-    path('articles/<str:slug>/delete/', delete_article, name='delete_article'),
+    path('', ArticleList.as_view(), name='index'),
+    path('articles/', ArticleList.as_view(), name='articles'),
+    path('articles/new/', ArticleCreate.as_view(), name='new_article'),
+    path('articles/<str:slug>/', ArticleDetail.as_view(), name='article'),
+    path('articles/<str:slug>/update/', ArticleUpdate.as_view(), name='update_article'),
+    path('articles/<str:slug>/delete/', ArticleDelete.as_view(), name='delete_article'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,89 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
 from .models import Article
 from .forms import ArticleForm
-from django.forms import ValidationError
-from django.utils.text import slugify
 
-def articles(request, *args, **kwargs):
-    objs = Article.objects.all()
-    return render(request, 'blog/index.html', {'articles': objs})
+def paginate(lst, page=1, count=10):
+    p = Paginator(lst, count)
+    try:
+        objects = p.page(page)
+    except PageNotAnInteger:
+        objects = p.page(1)
+    except EmptyPage:
+        objects = p.page(p.num_pages)
 
-def article(request, slug, *args, **kwargs):
-    article = get_object_or_404(Article, slug=slug)
-    return render(request, 'blog/article.html', {'article': article})
+    return objects
 
-def create_article(request, *args, **kwargs):
-    form = ArticleForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            obj = form.save()
-            return redirect(obj)
+# class-based views
 
-    return render(request, 'blog/add.html', {'form': form})
+class ArticleDetail(DetailView):
+    model = Article
+    template_name = 'blog/article.html'
+    context_object_name = 'article'
 
-def update_article(request, slug, *args, **qwargs):
-    article = get_object_or_404(Article, slug=slug)
-    form = ArticleForm(request.POST or None, instance=article)
-    if request.method == 'POST':
-        if form.is_valid():
-            obj = form.save()
-            return redirect(obj)
-    return render(request, 'blog/update.html', {'form': form})
+class ArticleList(ListView):
 
-def delete_article(request, slug, *args, **kwargs):
-    article = get_object_or_404(Article, slug=slug)
-    if request.method == 'POST':
-        article.delete()
-        return redirect('articles')
-    return render(request, 'blog/confirm.html', {'article': article})
+    def get(self, request):
+        page = request.GET.get('page')
+        articles = paginate(Article.objects.all(), page, count=3)
+        return render(request, 'blog/index.html', {'articles': articles})
+
+class ArticleCreate(CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'blog/add.html'
+
+    def get_success_url(self):
+        return reverse('article',args=(self.object.slug,))
+
+class ArticleUpdate(UpdateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'blog/update.html'
+
+    def get_success_url(self):
+        return reverse('article',args=(self.object.slug,))
+
+class ArticleDelete(DeleteView):
+    model = Article
+    template_name = 'blog/confirm.html'
+    success_url = reverse_lazy('articles')
+
+# # function-based views
+
+# def articles(request, *args, **kwargs):
+#     page = request.GET.get('page')
+#     objs = paginate(Article.objects.all(), page, count=3)
+#     return render(request, 'blog/index.html', {'articles': objs})
+
+# def article(request, slug, *args, **kwargs):
+#     article = get_object_or_404(Article, slug=slug)
+#     return render(request, 'blog/article.html', {'article': article})
+
+# def create_article(request, *args, **kwargs):
+#     form = ArticleForm(request.POST or None)
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             obj = form.save()
+#             return redirect(obj)
+
+#     return render(request, 'blog/add.html', {'form': form})
+
+# def update_article(request, slug, *args, **qwargs):
+#     article = get_object_or_404(Article, slug=slug)
+#     form = ArticleForm(request.POST or None, instance=article)
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             obj = form.save()
+#             return redirect(obj)
+#     return render(request, 'blog/update.html', {'form': form})
+
+# def delete_article(request, slug, *args, **kwargs):
+#     article = get_object_or_404(Article, slug=slug)
+#     if request.method == 'POST':
+#         article.delete()
+#         return redirect('articles')
+#     return render(request, 'blog/confirm.html', {'article': article})
